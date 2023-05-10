dd10355949fc957c86168afec0eb794e3b7e7074
Juan Camilo <jcjdlt@hotmail.com>
2023-05-10 17:29:08 -0500

    se cambia configuracion base de datos

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export const database = {
   connectionLimit: 10,
   host: process.env.DATABASE_HOST || "localhost",
   user: process.env.DATABASE_USER || "root",
-  password: process.env.DATABASE_PASSWORD || "root",
+  password: process.env.DATABASE_PASSWORD || "admin",
   database: process.env.DATABASE_NAME || "nails_by_johanna",
 };
 
