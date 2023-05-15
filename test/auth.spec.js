7484910909e164af06a69f3584760cf99e954d80
Juan Camilo <jcjdlt@hotmail.com>
2023-05-15 16:48:45 -0500

    Se corrigen datos de prueba para los test

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -24,7 +24,7 @@ describe("GET - POST /signup", () => {
 
         const postData = {
             fullname: "Prueba test",
-            email: "prueba@gmail.com",
+            email: "pruebatest@gmail.com",
             phone: 3107654542,
             password1: "ejemplo",
             password2: "ejemplo",
